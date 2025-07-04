09e8e8d44b0c1ac77926b70baeac13a6b11b0919
cesar <cesar.huari@gmail.com>
2025-07-04 00:10:27 -0300

    Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import ListOpc from './assets/components/Nav';
+import Box from '@mui/material/Box';
+import Header from './assets/components/Header';
 
 export default function BasicButtons() {
   return (
-    <>
-    <h1>Hola</h1>
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <Button>Viwe </Button>
-    </Stack>
-  </>
+    <Box sx={{
+    boxShadow: 3,
+    display: "flex",
+    flexDirection: "column"
+    }}>
+    <Header></Header>
+    <ListOpc></ListOpc>
+    </Box>
   );
 }
