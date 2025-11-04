e3f981a91610824c2c4c28bd45b398dd79c17e21
Teo <tadeosuarez0az@gmail.com>
2025-11-04 15:10:52 -0300

    no encuentra la bd en el deploy

--- backend/src/prisma/migrations/20251102210419_add_estado_reserva/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "EstadoReserva" AS ENUM ('PENDIENTE', 'ACEPTADA', 'RECHAZADA');
-
--- AlterTable
-ALTER TABLE "Reserva" ADD COLUMN     "estado" "EstadoReserva" NOT NULL DEFAULT 'PENDIENTE';
