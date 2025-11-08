6a872ae0b5860d3fd0a80f0978b6ede58e3a2f12
cesar <cesar.huari@gmail.com>
2025-11-08 01:01:31 -0300

    arreglo de entorno local

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 export const registerUser = async (userData) => {
   const res = await fetch(`${API_URL}/auth/register`, {
